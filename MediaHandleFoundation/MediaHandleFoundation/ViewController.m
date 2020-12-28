6277436db64bbc688ccfbafb01de1f0b5a72ed62
wiley <throughskybrim@gmail.com>
2020-12-28 18:12:04 +0800

    project add ffmpeg

--- MediaHandleFoundation/MediaHandleFoundation/ViewController.m
+++ MediaHandleFoundation/MediaHandleFoundation/ViewController.m
@@ -6,6 +6,7 @@
 //
 
 #import "ViewController.h"
+#include <libavutil/log.h>
 
 @interface ViewController ()
 
@@ -16,6 +17,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    
+    av_log(NULL, AV_LOG_ERROR, "error");
 }
 
 
